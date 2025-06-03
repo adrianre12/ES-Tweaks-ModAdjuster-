4855362800ca4025be577c2fdbd2f94bdbcbb1dc
adrianre12 <adrianrevill@hotmail.com>
2025-06-03 14:07:27 +0100

    Added Onezer

---
+++ Onezer.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox.Definitions;
+using System.Linq;
+using VRage.Game.Components;
+
+namespace ESTweaks
+{
+    [MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
+    class Onezer : MySessionComponentBase
+    {
+        public override void LoadData()
+        {
+            base.LoadData();
+            var allDefs = MyDefinitionManager.Static.GetAllDefinitions();
+            foreach (var componenet in allDefs.OfType<MyPhysicalItemDefinition>())
+            {
+                componenet.MinimalPricePerUnit = 1;
+            }
+        }
+    }
+}
